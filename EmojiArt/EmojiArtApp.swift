da93a88c7e6b01fae05cc834e7b892f385427eb0
David Burghoff <burghoffdavid@gmail.com>
2020-08-17 14:33:20 +0200

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by David Burghoff on 17.08.20.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
